a178b10ab36078e5329eab85cd9ea2227cc7a791
Szakr <szyty93@gmail.com>
2018-03-09 12:22:36 +0100

    nowa funkcja dodawania

--- Sortowanie/Sortowanie/Form1.cs
+++ Sortowanie/Sortowanie/Form1.cs
@@ -248,5 +248,12 @@ namespace Sortowanie
             czasSZPR.Text = string.Empty;
             sortWybRown();
         }
+        
+        private int nowaFunkcja()
+        {
+            int x = 42;
+            int y = 69;
+            return x+y;
+        }
     }
 }
